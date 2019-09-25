fabaea8995f4370100d9d6ad732c15eb4d1ed02e
kunyen811 <kunyen811@gmail.com>
2019-09-25 17:37:51 -0400

    Remove Outermost Parentheses

---
+++ 1021_removeOutermostParentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def removeOuterParentheses(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        previ = 0
+        res = ""
+        count = 0
+        for i, s in enumerate(S):
+            if s == '(':
+                count += 1
+            if s == ')':
+                count -= 1
+            if count == 0:
+                res += S[previ+1:i]
+                previ = i+1
+        return res
